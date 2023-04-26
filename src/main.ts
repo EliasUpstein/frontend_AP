0472b0ba42346a4f828c04f1f6754540b1d23037
Elias Upstein <eliasupstein@gmail.com>
2023-04-26 15:24:52 -0300

    persona-service + enviroment

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+export const environment = {
+  URL: 'http://localhost:8080/'
+};
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
