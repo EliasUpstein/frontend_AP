bfb2e4725ebce1aa0c818baaf0089b28446a75c9
Elias Upstein <eliasupstein@gmail.com>
2023-04-28 01:35:12 -0300

    generación de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { LoginComponent } from './components/login/login.component';
+import { AboutEditComponent } from './components/inicio/about/about-edit/about-edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: InicioComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'about-edit/:id', component: AboutEditComponent },
+  // {path:'nuevaexp', component: NewExperienciaComponent},
+  // {path:'editexp/:id', component: EditExperienciaComponent},
+  // { path: 'nuevaedu', component: NeweducacionComponent},
+  // { path: 'editedu/:id', component: EditeducacionComponent},
+  // { path: 'newskills', component: NewSkillsComponent},
+  // { path: 'editskill/:id', component: EditSkillComponent},
+  // { path: 'newproject', component: NewprojectComponent},
+  // { path: 'editproject/:id', component: EditprojectComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
