9fd328b3c7c1d6c711848e6799ab81a677790a46
Elias Upstein <eliasupstein@gmail.com>
2023-04-28 10:32:50 -0300

    actualización del enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,15 @@
 export const environment = {
+  firebase: {
+    projectId: 'frontend-ap-eliasupstein',
+    appId: '1:16137734792:web:bdbba93e17fdcded7fed3d',
+    storageBucket: 'frontend-ap-eliasupstein.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyDrpZTugdoszxB2ds-Oj5rJW0slVEiINCw',
+    authDomain: 'frontend-ap-eliasupstein.firebaseapp.com',
+    messagingSenderId: '16137734792',
+    measurementId: 'G-FB8CJ16VF4',
+  },
+
   production: true,
   URL: ''
 };
