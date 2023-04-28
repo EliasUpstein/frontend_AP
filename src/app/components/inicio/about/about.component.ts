d29dc19c5b7f0c2a6f0b90d6b608626e22653d79
Elias Upstein <eliasupstein@gmail.com>
2023-04-27 23:31:51 -0300

    componente about basico

--- src/app/components/inicio/about/about.component.ts
+++ src/app/components/inicio/about/about.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { persona } from 'src/app/models/persona.model';
+import { PersonaService } from 'src/app/services/persona.service';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+  persona: persona = new persona("", "", "");
+
+  // constructor(public personaService: PersonaService) { }
+
+  ngOnInit(): void {
+    // this.personaService.getPersona().subscribe(data => { this.persona = data })
+  }
 
 }
