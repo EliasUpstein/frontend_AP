c574008383133380f2ca8fe4353b6620207402eb
Elias Upstein <eliasupstein@gmail.com>
2023-04-28 10:52:41 -0300

    edit education template

--- src/app/components/inicio/educacion/educacion-edit/educacion-edit.component.ts
+++ src/app/components/inicio/educacion/educacion-edit/educacion-edit.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Educacion } from 'src/app/models/educacion';
+import { EducacionService } from 'src/app/services/educacion.service';
 @Component({
   selector: 'app-educacion-edit',
   templateUrl: './educacion-edit.component.html',
   styleUrls: ['./educacion-edit.component.css']
 })
-export class EducacionEditComponent {
+export class EducacionEditComponent implements OnInit {
+  educacion: Educacion = null;
 
+  constructor(
+    private educacionS: EducacionService,
+    private activatedRouter: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    const id = this.activatedRouter.snapshot.params['id'];
+    this.educacionS.detail(id).subscribe(
+      data => {
+        this.educacion = data;
+      }, err => {
+        alert("Error al modificar");
+        this.router.navigate(['']);
+      }
+    )
+  }
+
+  onUpdate(): void {
+    const id = this.activatedRouter.snapshot.params['id'];
+    this.educacionS.update(id, this.educacion).subscribe(
+      data => {
+        this.router.navigate(['']);
+      }, err => {
+        alert("Error al modificar la educacion");
+        this.router.navigate(['']);
+      }
+    )
+  }
 }
