497b93158ae3045b230375c253081708c41cc8bf
Elias Upstein <eliasupstein@gmail.com>
2023-04-28 12:13:52 -0300

    projects template

--- src/app/components/inicio/proyectos/proyectos.component.ts
+++ src/app/components/inicio/proyectos/proyectos.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Proyectos } from 'src/app/models/proyectos';
+import { ProyectosService } from 'src/app/services/proyectos.service';
+import { TokenService } from 'src/app/services/token.service';
 @Component({
   selector: 'app-proyectos',
   templateUrl: './proyectos.component.html',
   styleUrls: ['./proyectos.component.css']
 })
-export class ProyectosComponent {
+export class ProyectosComponent implements OnInit {
 
+  proyectos: Proyectos[] = [];
+
+  // constructor(private proyectosS: ProyectosService, private tokenService: TokenService) { }
+  isLogged = false;
+
+  ngOnInit(): void {
+    // this.cargarProyectos();
+    // if (this.tokenService.getToken()) {
+    //   this.isLogged = true;
+    // } else {
+    //   this.isLogged = false;
+    // }
+  }
+
+  cargarProyectos(): void {
+    // this.proyectosS.lista().subscribe(
+    //   data => {
+    //     this.proyectos = data;
+    //   }
+    // )
+  }
+
+  delete(id?: number) {
+    // if (id != undefined) {
+    //   this.proyectosS.delete(id).subscribe(
+    //     data => {
+    //       this.cargarProyectos();
+    //     }, err => {
+    //       alert("No se pudo eliminar");
+    //     }
+    //   )
+    // }
+  }
 }
