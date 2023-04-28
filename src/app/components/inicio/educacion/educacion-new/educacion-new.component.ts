6285a0040139d42d34edf8fc5563265ae158df74
Elias Upstein <eliasupstein@gmail.com>
2023-04-28 10:54:58 -0300

    new education edit

--- src/app/components/inicio/educacion/educacion-new/educacion-new.component.ts
+++ src/app/components/inicio/educacion/educacion-new/educacion-new.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Educacion } from 'src/app/models/educacion';
+import { EducacionService } from 'src/app/services/educacion.service';
 @Component({
   selector: 'app-educacion-new',
   templateUrl: './educacion-new.component.html',
   styleUrls: ['./educacion-new.component.css']
 })
-export class EducacionNewComponent {
+export class EducacionNewComponent implements OnInit {
+  nombreE: string;
+  descripcionE: string;
+
+  constructor(private educacionS: EducacionService, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  onCreate(): void {
+    const educacion = new Educacion(this.nombreE, this.descripcionE);
+    this.educacionS.save(educacion).subscribe(
+      data => {
+        alert("Educacion añadida correctamente");
+        this.router.navigate(['']);
+      }, err => {
+        alert("falló");
+        this.router.navigate(['']);
+      }
+    )
+  }
 
 }
