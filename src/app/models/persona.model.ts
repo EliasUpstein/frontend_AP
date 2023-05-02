fc0e946a8eb7d0a195ce846e78bf8ffe21d86da7
Elias Upstein <eliasupstein@gmail.com>
2023-05-02 16:05:48 -0300

    edit persona

--- src/app/models/persona.model.ts
+++ src/app/models/persona.model.ts
@@ -2,12 +2,14 @@ export class persona {
     id?: number;
     nombre: string;
     apellido: string;
+    profesion: string;
     descripcion: string;
     img: string;
 
-    constructor(nombre: string, apellido: string, descripcion: string, img: string) {
+    constructor(nombre: string, apellido: string, profesion: string, descripcion: string, img: string) {
         this.nombre = nombre;
         this.apellido = apellido;
+        this.profesion = profesion;
         this.descripcion = descripcion;
         this.img = img;
     }
